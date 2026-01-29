38ce9f21275458c37600c212379cc225914acf93
sanukathennakoon20@gmail.com <sanukathennakoon20@gmail.com>
2026-01-30 01:17:43 +0530

    createorde function added to the ordercontroller file

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -2,16 +2,22 @@ import Order from '../models/order.js';
 
 export async function createorder(req,res){
 
-    if(req.user==null){
-        res.json(
-            {
-                message:"you are not authorized please login"
-            }
-        )
-        return;
-    }
+    // if(req.user==null){
+    //     res.json(
+    //         {
+    //             message:"you are not authorized please login"
+    //         }
+    //     )
+    //     return;
+    // }
 
     try{
+
+        const user=req.user
+        if(user==null){
+            res.status(401).json({message:"Unauthorized"});
+            return;
+        }
 
         const orderList=await Order.find().sort({date:-1}).limit(1)
 
@@ -28,18 +34,86 @@ export async function createorder(req,res){
             newOrderId="CBC"+newOrderNumberInString;
             
         }
+        let customerName=req.body.customerName;
+        if(customerName==null)
+            {
+                customerName=user.firstName+" "+user.lastName;
+            }
+        
+        let phone=req.body.phone;
+        if(phone==null){
+            phone="not provided";
+        }
+
+        const itemsInRequest=req.body.items
+
+        if(itemsInRequest==null){
+            res.status(400).json({message:"Items are required to create an order"});
+            return;
+        }
+
+        if(!Array.isArray(itemsInRequest)){
+            res.status(400).json({message:"Items should be an array"});
+            return;
+        }
+
+        const itemToBeAdded=[];
+        let total=0
+
+        
+        for(let i=0;i<itemsInRequest.length;i++){
+            const item=itemsInRequest[i];
+
+            const product=await Product.findOne({productId:item.productId});
+            if(product==null){
+                res.status(400).json({
+                    code:"not-found",
+                    message:`Product with ID ${item.productId} not found`,
+                    productId:item.productId
+                    })
+                return;
+                
+            }
+            if(product.stock<item.quantity){
+                res.status(400).json(
+                    {
+                        code:"stock",
+                        message:`Insufficient stock for product ID ${item.productId}`, productId:item.productId, availableStock:product.stock
+                    })
+                return;
+            }
+
+            itemToBeAdded.push({
+                productId:product.productId,
+                productName:product.productName,
+                quantity:item.quantity,
+                price:product.price,
+                image:product.image[0]
+            });
+
+            total+=product.price*item.quantity;
+        }
+
         const newOrder=new Order({
             orderId:newOrderId,
-            Items:[],
-            customerName:req.body.customerName,
-            customerEmail:req.body.customerEmail,
-            phone:req.body.phone,
-            customerAddress:req.body.customerAddress,
-            total:req.body.total,
-            status:"Pending"   
+            Items:[itemToBeAdded],
+            customerName:customerName,
+            email:user.email,
+            phone:phone,
+            address:req.body.customerAddress,
+            total:total,
+             
         })
 
         const savedOrder =await  newOrder.save()
+
+        // for(let i=0;i<itemsToBeAdded.length;i++){
+        //     const item=itemsToBeAdded[i];
+        //     await Product.updateOne(
+        //         {productId:item.productId},
+        //         {$inc:{stock:-item.quantity}}
+        //     );
+        // }
 
         res.status(201).json(
             {
@@ -52,4 +126,8 @@ export async function createorder(req,res){
     }
 
 
+}
+
+export async function getOrders(req,res){
+    
 }
